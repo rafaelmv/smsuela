b246a8382058d87ce4d891c9016544738485703d
Rafa Miranda <rafayotuel@gmail.com>
2017-07-03 17:55:04 -0500

    Admin created for messages and models return text message

--- home/admin.py
+++ home/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from .models import Number
+from .models import Number, Message
 
 
 @admin.register(Number)
 class AdminClient(admin.ModelAdmin):
     list_display = ('phone_number', 'created_at')
     list_filter = ('phone_number', 'created_at')
+
+@admin.register(Message)
+class AdminMessage(admin.ModelAdmin):
+    list_display = ('text_message', 'created_at')
+    list_filter = ('text_message', 'created_at')
--- home/models.py
+++ home/models.py
@@ -13,3 +13,6 @@ class Message(models.Model):
     text_message = models.CharField(max_length=160)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.text_message
